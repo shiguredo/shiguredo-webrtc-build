5f80d91e08d89e71d9562ab508c398f8d79015c5
SUZUKI Tetsuya <szktty@shiguredo.jp>
2017-06-21 15:46:03 +0900

    コマンドを関数にまとめた

--- webrtc-build.go
+++ webrtc-build.go
@@ -402,6 +402,67 @@ func ArchiveAndroidProducts() {
 	Exec("zip", "-rq", androidArchiveZip, androidArchive)
 }
 
+type BuildScheme struct {
+	Debug, Release, Framework, Static bool
+}
+
+func Build(scheme BuildScheme) {
+	if isMac {
+		InitiOSBuild()
+		if scheme.Framework {
+			if scheme.Debug {
+				BuildiOSFramework("debug")
+			}
+			if scheme.Release {
+				BuildiOSFramework("release")
+			}
+		}
+		if scheme.Static {
+			if scheme.Debug {
+				BuildiOSStatic("debug")
+			}
+			if scheme.Release {
+				BuildiOSStatic("release")
+			}
+		}
+	} else {
+		if scheme.Debug {
+			BuildAndroidLibrary("debug")
+		}
+		if scheme.Release {
+			BuildAndroidLibrary("release")
+		}
+	}
+}
+
+func Archive() {
+	if isMac {
+		ArchiveiOSProducts()
+	} else {
+		ArchiveAndroidProducts()
+	}
+}
+
+func Clean() {
+	Exec("rm", "-rf", buildDir,
+		iOSArchive, iOSArchiveZip, androidArchive, androidArchiveZip,
+		"webrtc/src/testing/gmock",
+		"webrtc/src/testing/gtest",
+		filepath.Join(WebRTCDir, ".gclient"),
+		filepath.Join(WebRTCDir, ".gclient_entries"))
+}
+
+func Reset() {
+	dirs := []string{"webrtc/depot_tools",
+		"webrtc/src",
+		"webrtc/src/webrtc",
+		"webrtc/src/tools_webrtc"}
+	for _, dir := range dirs {
+		fmt.Printf("Discard changes of %s...\n", dir)
+		Exec("git", "-C", dir, "checkout", "--", ".")
+	}
+}
+
 var helpFlag = flag.Bool("h", false, "Print this message")
 
 func PrintHelp() {
@@ -474,73 +535,34 @@ func main() {
 		Sync()
 
 	case "build":
-		if isMac {
-			InitiOSBuild()
-			BuildiOSFramework("debug")
-			BuildiOSFramework("release")
-			BuildiOSStatic("debug")
-			BuildiOSStatic("release")
-		} else {
-			BuildAndroidLibrary("debug")
-			BuildAndroidLibrary("release")
-		}
+		Build(BuildScheme{Debug: true, Release: true, Framework: true, Static: true})
 
 	case "debug":
-		if isMac {
-			BuildiOSFramework("debug")
-			BuildiOSStatic("debug")
-		} else {
-			BuildAndroidLibrary("debug")
-		}
+		Build(BuildScheme{Debug: true, Framework: true, Static: true})
 
 	case "release":
-		if isMac {
-			BuildiOSFramework("release")
-			BuildiOSStatic("release")
-		} else {
-			BuildAndroidLibrary("release")
-		}
+		Build(BuildScheme{Release: true, Framework: true, Static: true})
 
 	case "framework-debug":
-		InitiOSBuild()
-		BuildiOSFramework("debug")
+		Build(BuildScheme{Debug: true, Framework: true})
 
 	case "framework-release":
-		InitiOSBuild()
-		BuildiOSFramework("release")
+		Build(BuildScheme{Release: true, Framework: true})
 
 	case "static-debug":
-		InitiOSBuild()
-		BuildiOSStatic("debug")
+		Build(BuildScheme{Debug: true, Static: true})
 
 	case "static-release":
-		InitiOSBuild()
-		BuildiOSStatic("release")
+		Build(BuildScheme{Release: true, Static: true})
 
 	case "dist":
-		if isMac {
-			ArchiveiOSProducts()
-		} else {
-			ArchiveAndroidProducts()
-		}
+		Archive()
 
 	case "clean":
-		Exec("rm", "-rf", buildDir,
-			iOSArchive, iOSArchiveZip, androidArchive, androidArchiveZip,
-			"webrtc/src/testing/gmock",
-			"webrtc/src/testing/gtest",
-			filepath.Join(WebRTCDir, ".gclient"),
-			filepath.Join(WebRTCDir, ".gclient_entries"))
+		Clean()
 
 	case "reset":
-		dirs := []string{"webrtc/depot_tools",
-			"webrtc/src",
-			"webrtc/src/webrtc",
-			"webrtc/src/tools_webrtc"}
-		for _, dir := range dirs {
-			fmt.Printf("Discard changes of %s...\n", dir)
-			Exec("git", "-C", dir, "checkout", "--", ".")
-		}
+		Reset()
 
 	case "help":
 		PrintHelp()
