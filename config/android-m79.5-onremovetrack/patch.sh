e95b700bf32bd9c4f1031ce63af1f022f6940174
SUZUKI Tetsuya <szktty@shiguredo.jp>
2019-12-20 16:01:47 +0900

    onremovetrack を含むパッチを追加

--- config/android-m79.5-onremovetrack/patch.sh
+++ config/android-m79.5-onremovetrack/patch.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-patch -buN $RTC_DIR/tools_webrtc/android/build_aar.py $PATCH_DIR/build_aar.py.diff
-patch -buN $RTC_DIR/sdk/android/BUILD.gn $PATCH_DIR/BUILD.gn.diff
+patch -buN -p1 -d $RTC_DIR < $PATCH_DIR/android.diff
 
