6775218fb6c53b4b654f34f8298cbb93bc97726d
SUZUKI Tetsuya <szktty@shiguredo.jp>
2020-02-20 15:50:00 +0900

    変更箇所をパッチにする

--- config/ios-m79.5.0-micext/patch.sh
+++ config/ios-m79.5.0-micext/patch.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 patch -buN $RTC_DIR/tools_webrtc/ios/build_ios_libs.py $PATCH_DIR/build_ios_libs.py.diff
-#patch -buN -p1 -d $RTC_DIR < $PATCH_DIR/mute.diff
+patch -buN -p1 -d $RTC_DIR < $PATCH_DIR/manual_audio_input.diff
 
-SRC_DIR=$PATCH_DIR/../src
-SDK_DIR=$RTC_DIR/sdk/objc
+#SRC_DIR=$PATCH_DIR/../src
+#SDK_DIR=$RTC_DIR/sdk/objc
 
-echo "cp $SRC_DIR/components/audio/* $SDK_DIR/components/audio"
-cp $SRC_DIR/components/audio/* $SDK_DIR/components/audio
+#echo "cp $SRC_DIR/components/audio/* $SDK_DIR/components/audio"
+#cp $SRC_DIR/components/audio/* $SDK_DIR/components/audio
 
-echo "cp $SRC_DIR/native/api/* $SDK_DIR/native/api"
-cp $SRC_DIR/native/api/* $SDK_DIR/native/api
+#echo "cp $SRC_DIR/native/api/* $SDK_DIR/native/api"
+#cp $SRC_DIR/native/api/* $SDK_DIR/native/api
 
-echo "cp $SRC_DIR/native/src/audio/* $SDK_DIR/native/src/audio"
-cp $SRC_DIR/native/src/audio/* $SDK_DIR/native/src/audio
+#echo "cp $SRC_DIR/native/src/audio/* $SDK_DIR/native/src/audio"
+#cp $SRC_DIR/native/src/audio/* $SDK_DIR/native/src/audio
